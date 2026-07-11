fb3b1834f93b02ec8889c5f4d8d504373ec13f66
Asangaziev Samat <sam1asangzv@gmail.com>
2026-07-11 16:26:59 +0600

    fix sql query

--- HomeWork39/HomeWork39.sql
+++ HomeWork39/HomeWork39.sql
@@ -1,15 +1,3 @@
--- Домашнее задание №39
--- Запросы с использованием подзапросов.
---
--- Используемые таблицы:
--- actions(action_id, product_id, supplier_id, action_date, qty, price)
--- products(product_id, product_name, category_id)
--- suppliers(supplier_id, supplier_name)
--- categories(category_id, category_name)
-
--- 1. Поставщики, которые в 2016 году поставили товаров на сумму больше,
--- чем поставщик IDT.
-
 SELECT
   s.supplier_name
 FROM suppliers s
@@ -33,7 +21,6 @@ WHERE (
 ORDER BY s.supplier_name;
 
 
--- 2. Товары, которые не поставлялись поставщиком IDT.
 
 SELECT
   p.product_name
@@ -43,16 +30,14 @@ WHERE NOT EXISTS (
   FROM actions a
   WHERE a.product_id = p.product_id
     AND a.supplier_id IN (
-    SELECT supplier_id
-    FROM suppliers
-    WHERE supplier_name = 'IDT'
-  )
+      SELECT supplier_id
+      FROM suppliers
+      WHERE supplier_name = 'IDT'
+    )
 )
 ORDER BY p.product_name;
 
 
--- 3. Категории товаров, которые были поставлены поставщиком IDT
--- летом 2016 года.
 
 SELECT DISTINCT
   c.category_name
